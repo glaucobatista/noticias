d68945e17550d9ada55ca204181f9ffb0f09b7f2
glauco <glaucogti@gmail.com>
2021-08-04 18:33:10 -0300

    View baseada em Classe e Paginator

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 app_name = 'blog'
 urlpatterns = [
- path('', views.publicacao_lista, name='publicacao_lista'),
+#  path('', views.publicacao_lista, name='publicacao_lista'),
+ path('', views.PublicacaoListView.as_view(), name='publicacao_lista'),
  path('<int:year>/<int:month>/<int:day>/<slug:publicacao>/', views.publicacao_detalhe, name='publicacao_detalhe'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
 from .models import Publicacao
 
 
+class PublicacaoListView(ListView):
+    queryset = Publicacao.objects.all()
+    context_object_name = 'publicacoes'
+    paginate_by = 1
+    template_name = 'blog/publicacao/lista.html'
+
 
 def publicacao_lista(request):
- publicacoes = Publicacao.objects.all()
- return render(request,
- 'blog/publicacao/lista.html',
- {'publicacoes': publicacoes})
+    object_list = Publicacao.objects.all()
+    paginator = Paginator(object_list, 5) # 5 publicações por página
+    page = request.GET.get('page')
+    try:
+        publicacoes = paginator.page(page)
+    except PageNotAnInteger:
+        publicacoes = paginator.page(1)
+    except EmptyPage:
+        publicacoes = paginator.page(paginator.num_pages)
+
+
+    return render(request, 'blog/publicacao/lista.html', {'page': page,'publicacoes': publicacoes})
+
 
 def publicacao_detalhe(request, year, month, day, publicacao):
     publicacao = get_object_or_404(Publicacao, 
